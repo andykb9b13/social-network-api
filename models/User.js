0def108a1348e23be4d5b2b8c32bf532b6125342
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-04-08 23:01:24 -0400

    created Thought and User Models, added user schema

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, Types } = require("mongoose");
+const Thought = require("./Thought");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email"],
+    },
+    thoughts: [],
+    friends: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
