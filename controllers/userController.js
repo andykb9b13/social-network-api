3d7c7065f8f89c5cd15141979b876875583868ef
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-04-10 06:35:28 -0400

    debugged / route for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const { User } = require("../models");
 module.exports = {
   async getUsers(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.find();
       res.status(200).json(users);
     } catch (err) {
       console.error(err);
